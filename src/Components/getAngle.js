34f111156b9059cc41c729dabc511e4de1a1dbaf
Rajat <rs20021023@gmail.com>
2022-03-04 19:05:01 +0530

    hourly weather ui completed

--- src/Components/getAngle.js
+++ src/Components/getAngle.js
@@ -3,15 +3,13 @@ async function getAngle(API_KEY,city,angles){
         const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&&appid=${API_KEY}`
         let response = await fetch(url,{mode:"cors"})
         let data = await response.json()
+        // console.log(data)
         angles.push(data[0].lat,data[0].lon)
         return angles;
     }
     catch(err){
         console.log("Error occured in getAngle.js " + err)
     }
-   
-    // console.log()
-
 
 }
 
--- src/app.js
+++ src/app.js
@@ -3,23 +3,16 @@ import { getAngle } from "./Components/getAngle";
 import { getDailyWeather } from "./Components/getDailyWeather";
 
 const API_KEY = "170e7d85c14723782ac20964a574ef47";
+let city = "Delhi";
+let angles = [];
 
-const btn = document.querySelector("button");
-const input = document.querySelector("input");
-const p = document.querySelector(".para")
-
-btn.addEventListener("click", () => {
-  let city = input.value;
-  let angles = [];
-
-  getAngle(API_KEY, city, angles).then((arr) => {
-    let lat = parseInt(arr[0]);
-    let long = parseInt(arr[1]);
-    getDailyWeather(API_KEY, lat, long).then((weatherData) => {
-     let todayWeather = weatherData.daily[0];
-     let weeklyWeather = weatherData.daily.splice(1,7)
-     p.innerText = `todays temperature is = ${todayWeather.temp.day}
-                    weekly weather is ${weeklyWeather}`
-    });
+getAngle(API_KEY, city, angles).then((arr) => {
+  let lat = parseInt(arr[0]);
+  let long = parseInt(arr[1]);
+  getDailyWeather(API_KEY, lat, long).then((weatherData) => {
+    let todayWeather = weatherData.daily[0];
+    let weeklyWeather = weatherData.daily.splice(1, 7);
   });
 });
+
+getAngle();
