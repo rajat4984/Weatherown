454ff254fafe7604a550abe3ce3bc47089315275
Rajat <rs20021023@gmail.com>
2022-03-05 12:50:27 +0530

    ui completed with responsiveness

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ const API_KEY = "170e7d85c14723782ac20964a574ef47";
 let city = "Delhi";
 let angles = [];
 
+const hamburger = document.querySelector(".hamburger");
+const searchBarContainer = document.querySelector(".search-bar-container");
+const searchIcon = document.querySelector(".search-icon")
+hamburger.addEventListener("click",()=>{
+  hamburger.classList.toggle("active");
+  searchBarContainer.classList.toggle("active");
+})
+
+searchIcon.addEventListener("click",()=>{
+  hamburger.classList.remove("active");
+  searchBarContainer.classList.remove("active");
+})
+
 getAngle(API_KEY, city, angles).then((arr) => {
   let lat = parseInt(arr[0]);
   let long = parseInt(arr[1]);
